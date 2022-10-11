c1ef37e99fcd83447e920d0ec39519213332d6ce
e1m7 <91698337+e1m7@users.noreply.github.com>
2022-10-12 00:21:32 +0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from PIL import Image
 
-# st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
