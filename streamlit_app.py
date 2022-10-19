99ee3bc7810c4717ce287689c51d69ca381012a5
e1m7 <91698337+e1m7@users.noreply.github.com>
2022-10-19 13:38:09 +0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,8 +58,7 @@ if selected == "Home":
     - Pandas [image](https://github.com/e1m7/streamlit-portfolio/blob/main/image/3.png)
     - Intro to Programming [image](https://github.com/e1m7/streamlit-portfolio/blob/main/image/4.png)
     """)
-    df = pd.DataFrame(
-    np.random.randn(10, 5), columns=('col %d' % i for i in range(5)))
+    df = pd.DataFrame(['11','22'], columns=('certificats')
     a1.table(df)
 
     a2.write("""
