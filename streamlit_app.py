8783f33e774be224371c1fd6bf937970984df3c2
e1m7 <91698337+e1m7@users.noreply.github.com>
2023-12-22 11:21:45 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -133,7 +133,10 @@ if selected == "Home":
     - французский B1
     - английский A1
     """)
-    a2.image(Image.open('image/rus.png')) a2.image(Image.open('image/fra.png')) a2.image(Image.open('image/eng.png'))
+    a2.image(Image.open('image/rus.png'))
+    a2.write(""" русский""") 
+    a2.image(Image.open('image/fra.png')) 
+    a2.image(Image.open('image/eng.png'))
 
 if selected == "Projects":
     st.title("Projects page")
